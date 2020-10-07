2c458959dfc7cfa5f197e7baa7504647bb186c16
Evgenij00 <49844107+Evgenij00@users.noreply.github.com>
2020-10-07 20:14:15 +0300

    built the architecture of the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { AddTodo } from './components/AddTodo'
+import './App.css';
+import { AddTodo } from './components/AddTodo';
 import { TodoList } from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='logo'>Todos</h1>
+      <h1>Todos</h1>
       <AddTodo />
       <TodoList />
     </div>
